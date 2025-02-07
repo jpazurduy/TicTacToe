39e8bbc9ed99d8318fdca5d05d8b70ac4ed1e83c
Jorge Azurduy <jpa.azurduy@gmail.com>
2025-02-06 20:08:17 -0400

    create game player

---
+++ TestSwiftUI.swift
@@ -0,0 +1,18 @@
+//
+//  TestSwiftUI.swift
+//  TicTacToe
+//
+//  Created by Jorge Azurduy on 1/28/25.
+//
+
+import SwiftUI
+
+struct TestSwiftUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestSwiftUI()
+}
